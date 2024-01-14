347df68b3bf213ffcf0604080838877701eb038a
가동재 <masterforce999@naver.com>
2024-01-14 23:43:27 +0900

    client components, server components'

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,19 +1,15 @@
+import Title from "../components/Title/Title";
+import CardItem from "../components/CardItem/CartItem";
+
 export default function Payment() {
   return (
     <div>
-      <h1>응 상품보관함</h1>
+      <Title/>
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CardItem />
+        <CardItem />
+        <CardItem />
       </div>
     </div>
   );
---
+++ app/components/Title/Title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1>응 상품보관함</h1>;
+};
+
+export default Title;

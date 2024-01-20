429385c51a8b35645bc7231636d4b3195470cd03
가동재 <masterforce999@naver.com>
2024-01-20 22:50:33 +0900

    next

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -14,7 +14,7 @@ export default function Payment() {
         <CardItem name={data[1]} />
         <CardItem name={data[2]}/>
       </div>
-      <Button color="red">빨강 버튼</Button>
+      <Button color="red">빨강 버튼.</Button>
       <Button color="blue">파랑 버튼</Button>
     </div>
   );
